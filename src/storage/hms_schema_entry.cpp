9809721065d80e04150952daa7bef863b281be1c
Oliver <86207103+thijs-s@users.noreply.github.com>
2026-05-14 13:17:48 +0200

    Update submodules in next branch (#7)
    * Update submodules
    
    * Fix visibility change in duckdb

--- src/storage/hms_schema_entry.cpp
+++ src/storage/hms_schema_entry.cpp
@@ -42,7 +42,7 @@ optional_ptr<CatalogEntry> HMSSchemaEntry::CreateFunction(CatalogTransaction tra
 }
 
 void HMSUnqualifyColumnRef(ParsedExpression &expr) {
-	if (expr.type == ExpressionType::COLUMN_REF) {
+	if (expr.GetExpressionType() == ExpressionType::COLUMN_REF) {
 		auto &colref = expr.Cast<ColumnRefExpression>();
 		auto name = std::move(colref.column_names.back());
 		colref.column_names = {std::move(name)};
